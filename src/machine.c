eafddf80c4fd0bb92bc2ffc4fa0aafd17fe48d34
Vitaliy Tsvayer <tsvayer@gmail.com>
2022-06-23 03:13:43 +0300

    3.Controlling the Flow: the GOTO solution!

--- src/machine.c
+++ src/machine.c
@@ -71,8 +71,18 @@ void destroy_ijvm() {
 
 }
 
-void increment_program_counter() {
-    current_program.program_counter += 1;
+void increment_program_counter(int16_t offset) {
+    current_program.program_counter += offset;
+}
+
+int8_t get_byte_operand() {
+    return (int8_t) get_text()[get_program_counter() + 1];
+}
+
+int16_t get_short_operand() {
+    byte_t operand1 = get_text()[get_program_counter() + 1];
+    byte_t operand2 = get_text()[get_program_counter() + 2];
+    return (int16_t) (operand1 * 0x100 + operand2);
 }
 
 void run() {
@@ -84,25 +94,65 @@ void run() {
 bool step(void) {
     switch (get_instruction()) {
         case OP_BIPUSH: {
-            increment_program_counter();
-            int8_t arg = (int8_t) get_text()[get_program_counter()];
+            int8_t arg = get_byte_operand();
             push_stack(current_program.stack, arg);
+            increment_program_counter(2);
             log("BIPUSH %d\n", arg);
             break;
         }
-        case OP_DUP:
+        case OP_DUP: {
+            word_t arg = top_stack(current_program.stack);
+            push_stack(current_program.stack, arg);
+            increment_program_counter(1);
             log("DUP\n");
             break;
+        }
         case OP_ERR:
+            increment_program_counter(1);
             log("ERR\n");
             break;
-        case OP_GOTO:
-            log("GOTO\n");
+        case OP_GOTO: {
+            int16_t offset = get_short_operand();
+            log("GOTO %d\n", offset);
+            increment_program_counter(offset);
             break;
+        }
+        case OP_IFEQ: {
+            word_t arg = pop_stack(current_program.stack);
+            int16_t offset = get_short_operand();
+            if (arg != 0) {
+                offset = 3;
+            }
+            increment_program_counter(offset);
+            log("IFEQ\n");
+            break;
+        }
+        case OP_IFLT: {
+            word_t arg = pop_stack(current_program.stack);
+            int16_t offset = get_short_operand();
+            if (arg >= 0) {
+                offset = 3;
+            }
+            increment_program_counter(offset);
+            log("IFLT\n");
+            break;
+        }
+        case OP_ICMPEQ: {
+            word_t arg1 = pop_stack(current_program.stack);
+            word_t arg2 = pop_stack(current_program.stack);
+            int16_t offset = get_short_operand();
+            if (arg1 != arg2) {
+                offset = 3;
+            }
+            increment_program_counter(offset);
+            log("ICMPEQ\n");
+            break;
+        }
         case OP_IADD: {
             word_t arg2 = pop_stack(current_program.stack);
             word_t arg1 = pop_stack(current_program.stack);
             push_stack(current_program.stack, arg1 + arg2);
+            increment_program_counter(1);
             log("IADD\n");
             break;
         }
@@ -110,6 +160,7 @@ bool step(void) {
             word_t arg2 = pop_stack(current_program.stack);
             word_t arg1 = pop_stack(current_program.stack);
             push_stack(current_program.stack, arg1 - arg2);
+            increment_program_counter(1);
             log("ISUB\n");
             break;
         }
@@ -117,6 +168,7 @@ bool step(void) {
             word_t arg2 = pop_stack(current_program.stack);
             word_t arg1 = pop_stack(current_program.stack);
             push_stack(current_program.stack, arg1 & arg2);
+            increment_program_counter(1);
             log("IAND\n");
             break;
         }
@@ -124,22 +176,16 @@ bool step(void) {
             word_t arg2 = pop_stack(current_program.stack);
             word_t arg1 = pop_stack(current_program.stack);
             push_stack(current_program.stack, arg1 | arg2);
+            increment_program_counter(1);
             log("IOR\n");
             break;
         }
-        case OP_IFEQ:
-            log("IFEQ\n");
-            break;
-        case OP_IFLT:
-            log("IFLT\n");
-            break;
-        case OP_ICMPEQ:
-            log("ICMPEQ\n");
-            break;
         case OP_IINC:
+            increment_program_counter(1);
             log("IINC\n");
             break;
         case OP_ILOAD:
+            increment_program_counter(1);
             log("ILOAD\n");
             break;
         case OP_IN: {
@@ -147,32 +193,40 @@ bool step(void) {
             if (input == EOF)
                 input = '0';
             push_stack(current_program.stack, input);
+            increment_program_counter(1);
             log("IN\n");
             break;
         }
         case OP_INVOKEVIRTUAL:
+            increment_program_counter(1);
             log("INVOKEVIRTUAL\n");
             break;
         case OP_IRETURN:
+            increment_program_counter(1);
             log("IRETURN\n");
             break;
         case OP_ISTORE:
+            increment_program_counter(1);
             log("ISTORE\n");
             break;
         case OP_LDC_W:
+            increment_program_counter(1);
             log("LDC_W\n");
             break;
         case OP_NOP:
+            increment_program_counter(1);
             log("NOP\n");
             break;
         case OP_OUT: {
             word_t arg = pop_stack(current_program.stack);
             putc(arg, current_program.output);
+            increment_program_counter(1);
             log("OUT\n");
             break;
         }
         case OP_POP: {
             pop_stack(current_program.stack);
+            increment_program_counter(1);
             log("POP\n");
             break;
         }
@@ -181,10 +235,12 @@ bool step(void) {
             word_t arg1 = pop_stack(current_program.stack);
             push_stack(current_program.stack, arg2);
             push_stack(current_program.stack, arg1);
+            increment_program_counter(1);
             log("SWAP\n");
             break;
         }
         case OP_WIDE:
+            increment_program_counter(1);
             log("WIDE\n");
             break;
         case OP_HALT: {
@@ -197,9 +253,7 @@ bool step(void) {
             log("Unknown Instruction\n");
             break;
         }
-
     }
-    increment_program_counter();
     return true;
 }
 
